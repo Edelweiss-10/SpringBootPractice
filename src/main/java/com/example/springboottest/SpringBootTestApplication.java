e429fd501064ee0877f282ec053afc0675cbf020
Edelweiss-10 <maltsev.k@ajax.systems>
2022-06-22 16:51:47 +0300

    alliases annotations of auto configure

--- src/main/java/com/example/springboottest/SpringBootTestApplication.java
+++ src/main/java/com/example/springboottest/SpringBootTestApplication.java
@@ -1,15 +1,17 @@
 package com.example.springboottest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 
-
-@Configuration
-@SpringBootApplication
+//@ComponentScan(basePackageClasses = {com.example.springboottest.service.MyAccountService.class, com.example.springboottest.RiskAssessor.class})
+//@ComponentScan(value = {"com.example.springboottest"})
+@ComponentScan
+@EnableAutoConfiguration
+//@SpringBootApplication
 public class SpringBootTestApplication {
 
     public static void main(String[] args) {
